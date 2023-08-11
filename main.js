562c9760a3d0948d390f46560c6763fe94aa38ba
Vinícius Blandy <viny19381@hotmail.com>
2023-08-11 20:30:24 -0300

    atualizando codigo

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-const xhttp = new XMLHttpRequest()
-const endpoint = 'https://api.github.com/users/ecblandy'
+const xhttp = new XMLHttpRequest();
+const endpoint = 'https://api.github.com/users/ecblandy';
 
-xhttp.open('GET', endpoint, true)
-xhttp.responseType = 'json'
-xhttp.onreadystatechange = () => {
-  if (xhttp.readyState === 4  && xhttp.status === 200) {
-    respostaServidor = xhttp.response
-    pegarDados(respostaServidor)
-
-    function pegarDados (dados) {
-      document.querySelector('.profile-avatar').src = dados.avatar_url
-      document.querySelector('.profile-name').textContent = dados.name
-      document.querySelector('.profile-username').textContent = `@${dados.login}`
-      document.getElementById('repositorios').textContent = dados.public_repos
-      document.getElementById('seguidores').textContent = dados.followers
-      document.getElementById('seguindo').textContent = dados.following
-      document.querySelector('.profile-link').href = dados.html_url
+try {
+  xhttp.open('GET', endpoint, true);
+  xhttp.responseType = 'json';
+  
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      const respostaServidor = xhttp.response;
+      try {
+        pegarDados(respostaServidor);
+      } catch (error) {
+        console.error('Erro ao manipular dados:', error);
+      }
     }
-  }
+  };
+  
+  xhttp.send();
+} catch (error) {
+  console.error('Erro ao fazer a requisição:', error);
 }
 
-xhttp.send()
+function pegarDados(dados) {
+  document.querySelector('.profile-avatar').src = dados.avatar_url;
+  document.querySelector('.profile-name').textContent = dados.name;
+  document.querySelector('.profile-username').textContent = `@${dados.login}`;
+  document.getElementById('repositorios').textContent = dados.public_repos;
+  document.getElementById('seguidores').textContent = dados.followers;
+  document.getElementById('seguindo').textContent = dados.following;
+  document.querySelector('.profile-link').href = dados.html_url;
+}
+
